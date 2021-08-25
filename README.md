9c01618eda9dcbbb4d794eed8592135931e4ebdf
Alex <alexd@axosoft.com>
2021-08-24 22:10:06 -0700

    adding files to the git pull branch

---
+++ README.md
@@ -0,0 +1,1 @@
+# Git Pull Main Demo
---
+++ git-pull.js
@@ -0,0 +1,6 @@
+// This is the file we will make the pull from to the main branch. 
+
+
+pullText = () => {
+    return "This is the file we will make the pull from to the main branch" 
+}
